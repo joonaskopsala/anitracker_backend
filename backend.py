fb18f2f1eafa88c9f2712edd684f19e3e07d047e
Joonas <joonas.kopsala@gmail.com>
2024-10-25 23:59:22 +0300

    fetch all anime

--- backend.py
+++ backend.py
@@ -8,27 +8,25 @@ CORS(app)
 
 JIKAN_API_URL = 'https://api.jikan.moe/v4/seasons/now'
 
-def fetch_airing_anime():
-    response = requests.get(JIKAN_API_URL)
-    if response.status_code == 200:
-        data = response.json()
-        airing_anime = []
-        
-        for anime in data['data']:
-            airing_anime.append({
-                'title': anime['title'],
-                'episode': anime.get('episodes', 'Unknown'),
-                'airing_start': anime['aired']['from'],
-                'cover_image': anime['images']['jpg']['large_image_url']
-            })
-        
-        return airing_anime
-    else:
-        return {'error': 'Failed to fetch data from Jikan API'}, response.status_code
+def fetch_all_airing_anime():
+    all_anime = []
+    page = 1
+    while True:
+        response = requests.get(f'{JIKAN_API_URL}?page={page}')
+        if response.status_code == 200:
+            data = response.json().get('data', [])
+            if not data:
+                break
+            all_anime.extend(data)
+            page += 1
+        else:
+            break
+    
+    return all_anime
 
 @app.route('/airing_anime', methods=['GET'])
 def airing_anime():
-    data = fetch_airing_anime()
+    data = fetch_all_airing_anime()
     return jsonify(data)
 
 if __name__ == '__main__':
