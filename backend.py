d049c52accb55a0c457a2130c81b6e45c70f1143
Joonas <joonas.kopsala@gmail.com>
2024-10-25 23:21:36 +0300

    Change api to a better one

--- backend.py
+++ backend.py
@@ -1,35 +1,30 @@
-from flask import Flask, jsonify
 import requests
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 
 CORS(app)
 
-ANILIST_API_URL = 'https://graphql.anilist.co'
+JIKAN_API_URL = 'https://api.jikan.moe/v4/seasons/now'
 
 def fetch_airing_anime():
-    query = '''
-    query {
-      Page {
-        airingSchedules {
-          media {
-            title {
-              english
-              native
-            }
-            coverImage {
-              large
-            }
-          }
-          episode
-          airingAt
-        }
-      }
-    }
-    '''
-    response = requests.post(ANILIST_API_URL, json={'query': query})
-    return response.json()
+    response = requests.get(JIKAN_API_URL)
+    if response.status_code == 200:
+        data = response.json()
+        airing_anime = []
+        
+        for anime in data['data']:
+            airing_anime.append({
+                'title': anime['title'],
+                'episode': anime.get('episodes', 'Unknown'),
+                'airing_start': anime['aired']['from'],
+                'cover_image': anime['images']['jpg']['large_image_url']
+            })
+        
+        return airing_anime
+    else:
+        return {'error': 'Failed to fetch data from Jikan API'}, response.status_code
 
 @app.route('/airing_anime', methods=['GET'])
 def airing_anime():
